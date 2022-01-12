7cb49e4536fec5c0405b238287b1bd61d7ac12ec
WooDooWooDa <jeremie-bouchard@hotmail.fr>
2022-01-12 00:32:29 -0500

    level select

---
+++ gamejam2022/Assets/Scripts/Characters/Inventory.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] private Dictionary<string, GameObject> inventory;
+
+    void Start()
+    {
+        inventory = new Dictionary<string, GameObject>();
+    }
+
+    public void Add(string key, GameObject obj)
+    {
+        inventory.Add(key, obj);
+    }
+
+    public GameObject Contains(string key)
+    {
+        if (inventory.TryGetValue(key, out var value)) {
+            return value;
+        }
+        return null;
+    }
+
+}
