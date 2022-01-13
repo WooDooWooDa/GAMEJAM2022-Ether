1e0167be6df6bfa1a86da6c2183af03876f1d500
Lee31416 <tom_pig9@hotmail.com>
2022-01-12 21:46:35 -0500

    Did a basic main menu

---
+++ gamejam2022/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame() 
+    {
+        Application.Quit();    
+    }
+
+    public void LoadGame()
+    {
+        //TODO 
+    }
+
+}
