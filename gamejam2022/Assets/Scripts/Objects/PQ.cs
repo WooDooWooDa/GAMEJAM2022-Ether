0e35630353ef60f10109a19eef8dbb3bc79c91de
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-01-13 13:06:15 -0500

    Updated Main Mixer to fade music on interactions

--- gamejam2022/Assets/Scripts/Objects/PQ.cs
+++ gamejam2022/Assets/Scripts/Objects/PQ.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PQ : Interactable
+public class PQ : InteractableObject
 {
     public override void Interact(GameObject player)
     {
-        GetComponent<AudioSource>().Play();
+        PlayInteractSound();
         var inventory = player.GetComponent<Inventory>();
         if (!inventory.GetInventoryItem("pq"))
             inventory.Add("pq", gameObject);
