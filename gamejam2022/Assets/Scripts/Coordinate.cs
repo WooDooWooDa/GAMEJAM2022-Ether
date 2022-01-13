a9b615a8f934ef05a656e71d20a074958fb19b46
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-01-13 01:32:03 -0500

    Made Dracula room and door interaction

---
+++ gamejam2022/Assets/Scripts/Coordinate.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coordinate : MonoBehaviour
+{
+    [SerializeField] public float xCoordinate;
+    [SerializeField] public float yCoordinate;
+}
