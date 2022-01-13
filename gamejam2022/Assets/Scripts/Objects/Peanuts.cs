347735c2ce4dfc25034192143b4488320c199793
Joshua Leblanc <leblancjoshua077@gmail.com>
2022-01-13 13:29:08 -0500

    Fixed object prefabs

--- gamejam2022/Assets/Scripts/Objects/Peanuts.cs
+++ gamejam2022/Assets/Scripts/Objects/Peanuts.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Peanuts : Interactable
+public class Peanuts : InteractableObject
 {
     public override void Interact(GameObject player)
     {
+        PlayInteractSound();
         var inventory = player.GetComponent<Inventory>();
         if (!inventory.GetInventoryItem("peanut"))
             inventory.Add("peanut", gameObject);
